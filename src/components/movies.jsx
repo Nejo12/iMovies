cc60505c133d124600037fe062a1003961d38583
Gabriel <olaniyi_g@hotmail.com>
2019-05-17 14:16:43 +0200

    Pagination: not as easy as thought, but...

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
+import { paginate } from "../utils/paginate";
 import Like from "./common/like";
 import Pagination from "./common/pagination";
 
 class Movies extends Component {
   state = {
     movies: getMovies(),
-    pageSize: 3
+    pageSize: 4,
+    currentPage: 1
   };
 
   handleDelete = movie => {
@@ -23,13 +25,16 @@ class Movies extends Component {
   };
 
   handlePageChange = page => {
-    console.log("page changed: ", page);
+    this.setState({ currentPage: page });
   };
 
   render() {
     const { length: count } = this.state.movies;
+    const { pageSize, currentPage, movies: allMovies } = this.state;
 
     if (count === 0) return <p>There are no movies in Database</p>;
+
+    const movies = paginate(allMovies, currentPage, pageSize);
 
     return (
       <React.Fragment>
@@ -46,7 +51,7 @@ class Movies extends Component {
             </tr>
           </thead>
           <tbody>
-            {this.state.movies.map(movie => (
+            {movies.map(movie => (
               <tr key={movie._id}>
                 <td>{movie.title}</td>
                 <td>{movie.genre.name}</td>
@@ -72,7 +77,8 @@ class Movies extends Component {
         </table>
         <Pagination
           itemsCount={count}
-          pageSize={this.state.pageSize}
+          pageSize={pageSize}
+          currentPage={currentPage}
           onPageChange={this.handlePageChange}
         />
       </React.Fragment>
