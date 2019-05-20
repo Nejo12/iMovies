10b4f000027c13ebfa4973dea8d337764e81f8e8
Gabriel <olaniyi_g@hotmail.com>
2019-05-20 19:32:34 +0200

    Simply Routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
 import "./App.css";
-import Movies from "./components/movies";
 import NavBar from "./components/common/navBar";
+import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
 
-function App() {
-  return (
-    <main className="container">
-      <NavBar />
-      <Movies />
-    </main>
-  );
+class App extends Component {
+  render() {
+    return (
+      <main className="container">
+        <Switch>
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect exact from="/" to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+        {/* <NavBar />
+        <Movies /> */}
+      </main>
+    );
+  }
 }
 
 export default App;
