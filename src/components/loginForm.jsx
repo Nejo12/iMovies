6bb60670c717cd60e79f7a471e6e08ad518e5d9b
Gabriel <olaniyi_g@hotmail.com>
2019-05-22 10:24:36 +0200

    storing login the jwt

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -22,7 +22,10 @@ class LoginForm extends Form {
     // call the server...
     try {
       const { data } = this.state;
-      await login(data.username, data.password);
+      const { data: jwt } = await login(data.username, data.password);
+      // console.log(jwt);
+      localStorage.setItem("token", jwt);
+      this.props.history.push("/");
     } catch (ex) {
       if (ex.response && ex.response.status === 400) {
         const errors = { ...this.state.errors };
