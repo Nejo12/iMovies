0987bfd0b01dbc74ba1e6d793072db2f75681f38
Gabriel <olaniyi_g@hotmail.com>
2019-05-22 16:37:47 +0200

    calling protected api endpoint

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 import logger from "./logService";
-import auth from "./authService";
+// import auth from "./authService";
 import { toast } from "react-toastify";
 
-axios.defaults.headers.common["x-auth-token"] = auth.getJwt();
+// axios.defaults.headers.common["x-auth-token"] = auth.getJwt();
 
 axios.interceptors.response.use(null, error => {
   const expectedError =
@@ -24,9 +24,14 @@ axios.interceptors.response.use(null, error => {
   return Promise.reject(error);
 });
 
+function setJwt(jwt) {
+  axios.defaults.headers.common["x-auth-token"] = jwt;
+}
+
 export default {
   get: axios.get,
   post: axios.post,
   put: axios.put,
-  delete: axios.delete
+  delete: axios.delete,
+  setJwt
 };
